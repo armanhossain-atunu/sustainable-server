3830540b67d035c1d7d739ab4869f6e54a6dbe15
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-04-11 01:30:40 +0600

    user interface and user moder change

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,6 +6,7 @@ const userSchema = new Schema<TUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    photo:{ type: String, required: true },
     password: { type: String, required: true, select: false },
     role: {
       type: String,
--- src/types/user.interface.ts
+++ src/types/user.interface.ts
@@ -1,6 +1,7 @@
 export interface TUser {
   name: string;
   email: string;
+  photo:string;
   password?: string;
   role: 'admin' | 'user';
 }
